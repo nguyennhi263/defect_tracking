ece5eddb5a1bb46a778cf81c86723fad47093553
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-05-03 20:38:27 +0700

    function show unit places on map

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -37,7 +37,9 @@ jQuery(document).ready(function ($) {
                         }),
                         success: function (data, textStatus){
                            // $("#div1").html(data);
-                           alert(data);
+                           // alert(data);
+                            // If save successfully, load saved unit places
+                            render_unit_places(relativeX, relativeY, place_name_val);
                         }
                     });
                 // Hide place name form
@@ -57,6 +59,16 @@ jQuery(document).ready(function ($) {
             relativeX = parseInt(e.pageX - offset.left);
             relativeY = parseInt(e.pageY - offset.top);
         });
+
+        // set unit places to map
+        function render_unit_places(pointX, pointY, place_name, place_id){
+            console.log("testing");
+            html = '<div class="unit-place" style="top:'+pointY+'px; left:'+pointX+'px;">';
+            html += '<div class="place-info"><span class="place-name">'+place_name+'</span>';
+            html += '<span class="place-delete" data-delete-id="'+place_id+'" title="Delete this place"><i class="fa fa-trash" aria-hidden="true"></i></span>';
+            html += '</div></div>';
+            map.append(html);
+        }
     }
     get_coordinates();
 });
