6a21e0955d2b21dc53d271e1bb23a07e1f44462f
nguyennhi263 <nguyennhi263@gmail.com>
2018-05-03 21:48:59 +0700

    --

--- webroot/js/myajax.js
+++ webroot/js/myajax.js
@@ -1,17 +1,21 @@
 jQuery(document).ready(function ($) {
-
+    
 });
 
 function getListDefectPlace($id) {
+    var result;
     $.ajax({
         dataType: "html",
         method: "GET",
         //dataType:"json",
         evalScripts: true,
-        url: "/defect-places/viewByUnitType/" + $id + ".json",
+        url: "/defect_tracking/defect-places/viewByUnitType/" + $id + ".json",
         data: ({}),
         success: function (data, textStatus) {
-            return data;
+        //console.log(data);
+            result = data;
+           //alert(data);
         }
     });
+    return result;
 }
