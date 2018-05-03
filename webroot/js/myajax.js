7e2b0bf3e403abd2d6c6d3f30e99f3e9251a08c2
nguyennhi263 <nguyennhi263@gmail.com>
2018-05-03 19:53:57 +0700

    view defec place by id -ajax

--- webroot/js/myajax.js
+++ webroot/js/myajax.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function ($) {
-	function getListDefectPlace(){
+	function getListDefectPlace($id){
 		$.ajax({
                         dataType: "html",
                         method:"GET",
                         //dataType:"json",
                         evalScripts: true,
-                        url: "/defect-places/viewByUnitType/",
+                        url: "/defect-places/viewByUnitType/"+id+".json",
                         data: ({
                         }),
                         success: function (data, textStatus){
